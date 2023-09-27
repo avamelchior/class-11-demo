c1c52ee3d565554c332b2f4773d37edc50332911
Ava Melchior <avacmelchior@gmail.com>
2023-09-27 09:04:32 -0500

    attempt at js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-'use strict'
+'use strict';
+
 console.log('app.js is running!');
 
 let imageContainer = document.querySelector('section');
@@ -6,13 +7,12 @@ let resultButton = document.querySelector('section + div');
 let image1 = document.getElementById('imageOne');
 let image2 = document.getElementById('imageTwo');
 let image3 = document.getElementById('imageThree');
-console.log({imageContainer,resultButton,image1,image2});
 
 let clicks = 0;
 let maxAttemptsAllowed = 10;
 Animals.allAnimalsArray = [];
 
-function Animals(name, src){
+function Animals(name, src) {
     this.name = name;
     this.src = src;
     this.views = 0;
@@ -21,67 +21,63 @@ function Animals(name, src){
     Animals.allAnimalsArray.push(this);
 }
 
-function getRandomNumber(){
-    return Math.round(Math.random() * Animals.allAnimalsArray.length);
+function getRandomNumber() {
+    return Math.floor(Math.random() * Animals.allAnimalsArray.length);
 }
 
-function renderAnimals(){
-    let animal1 = getRandomNumber();
-    let animal2 = getRandomNumber();
-    let animal3 = getRandomNumber();
-    console.log(animal1,animal2,animal3);
+function renderAnimals() {
+    let animal1, animal2, animal3;
 
-    while(animal1 === animal2 === animal3) {
+    do {
+        animal1 = getRandomNumber();
         animal2 = getRandomNumber();
         animal3 = getRandomNumber();
-    }
+    } while (animal1 === animal2 || animal1 === animal3 || animal2 === animal3);
 
     image1.src = Animals.allAnimalsArray[animal1].src;
-image2.src = Animals.allAnimalsArray[animal2].src;
-image3.src = Animals.allAnimalsArray[animal3].src;
+    image2.src = Animals.allAnimalsArray[animal2].src;
+    image3.src = Animals.allAnimalsArray[animal3].src;
 
-image1.alt = Animals.allAnimalsArray[animal1];
-image2.alt = Animals.allAnimalsArray[animal2];
-image3.alt = Animals.allAnimalsArray[animal3]
+    image1.alt = Animals.allAnimalsArray[animal1].name;
+    image2.alt = Animals.allAnimalsArray[animal2].name;
+    image3.alt = Animals.allAnimalsArray[animal3].name;
 
-Animals.allAnimalsArray[animal1].views++;
-Animals.allAnimalsArray[animal2].views++;
-Animals.allAnimalsArray[animal3].views++;
+    Animals.allAnimalsArray[animal1].views++;
+    Animals.allAnimalsArray[animal2].views++;
+    Animals.allAnimalsArray[animal3].views++;
 }
 
-
-
-function handleAnimalClick(event){
+function handleAnimalClick(event) {
     console.log('proof of life!', event);
-    if(event.target === imageContainer){
-        alert('Please click on an image!')
+    if (event.target === imageContainer) {
+        alert('Please click on an image!');
     }
 
-clicks++;
+    clicks++;
 
-let clickAnimal = event.target.alt;
-console.log('animal name',clickAnimal);
-for(let i = 0; i < Animals.allAnimalsArray.length; i++){
-    if(clickAnimal === Animals.allAnimalsArray[i].name){
-    Animals.allAnimalsArray[i].click++;
-    break;
+    let clickAnimal = event.target.alt;
+    console.log('animal name', clickAnimal);
+    for (let i = 0; i < Animals.allAnimalsArray.length; i++) {
+        if (clickAnimal === Animals.allAnimalsArray[i].name) {
+            Animals.allAnimalsArray[i].click++;
+            break;
+        }
+    }
+
+    if (clicks === maxAttemptsAllowed) {
+        imageContainer.removeEventListener('click', handleAnimalClick);
+        resultButton.addEventListener('click', renderResults);
+        imageContainer.className = 'no-voting';
+    } else {
+        renderAnimals();
     }
 }
 
-if(clicks === maxAttemptsAllowed){
-    imageContainer.removeEventListener('click', handleAnimalClick);
-    resultButton.addEventListener('click', renderResults);
-    imageContainer.className = 'no-voting';
-} else {
-    renderAnimals();
-}
-}
-
-function renderResults(){
+function renderResults() {
     console.log('proof of life in the render results');
     let ul = document.querySelector('ul');
 
-    for(let i = 0; i < Animals.allAnimalsArray.length; i++){
+    for (let i = 0; i < Animals.allAnimalsArray.length; i++) {
         let li = document.createElement('li');
         li.textContent = `${Animals.allAnimalsArray[i].name} had ${Animals.allAnimalsArray[i].views} views and was clicked on ${Animals.allAnimalsArray[i].click} times`;
         ul.appendChild(li);
@@ -111,3 +107,7 @@ new Animals('wine-glass', 'images/wine-glass.jpg');
 new Animals('Bunny', 'images/bunny.jpg');
 new Animals('Dog', 'images/doggggggo.jpg');
 new Animals('Cat', 'images/kittycat.jpg');
+
+
+renderAnimals();
+imageContainer.addEventListener('click', handleAnimalClick);
